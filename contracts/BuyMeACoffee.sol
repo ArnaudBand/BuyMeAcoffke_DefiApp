b77354a2dbd2fa674bc99a18b5debd96c5703541
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-24 09:28:38 +0300

    Delete the boilerplate and create a contract.

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    //  Event to emit when a Memo is created
+
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct.
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // List of all memos received from friends.
+
+    Memo[] memos;
+
+    // Address of contract deployer.
+
+    address payable owner;
+
+    // Deploy logic.
+
+    constructor() {
+        owner = payable(msg.sender);  
+    }
+
+    /**
+     * @dev buy a coffee for the contract owner
+     * @param _name name of coffee buyer
+     * @param _message a nice message from the coffe buyer
+     */
+
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "You can't buy a coffee with 0 ETH");
+
+
+        // Add memos to the storage
+        memos.push(Memo (
+                msg.sender,
+                block.timestamp,
+                _name,
+                _message
+        ));
+
+        // Emit a log event when a new memo is created!
+
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+     */
+
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrieve all the memos received and stored on the blockchain
+     */
+
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
+}
